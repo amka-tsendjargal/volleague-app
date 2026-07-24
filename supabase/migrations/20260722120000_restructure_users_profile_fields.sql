91bfba70624f0f4dc95c81e6d8fb6d85f36ab7e9
Aiden-W1224 <aidenwong40@gmail.com>
2026-07-24 13:26:11 -0600

    remove email

--- supabase/migrations/20260722120000_restructure_users_profile_fields.sql
+++ supabase/migrations/20260722120000_restructure_users_profile_fields.sql
@@ -7,7 +7,6 @@
 alter table public.users drop column name;
 alter table public.users add column first_name   varchar(255) not null;
 alter table public.users add column last_name    varchar(255) not null;
-alter table public.users add column email        varchar(255) not null;
 alter table public.users add column phone_number varchar(32);
 
 -- ------------------------------------------------------------
@@ -19,12 +18,11 @@ language plpgsql
 security definer set search_path = public
 as $$
 begin
-  insert into public.users (id, first_name, last_name, email, phone_number)
+  insert into public.users (id, first_name, last_name, phone_number)
   values (
     new.id,
     new.raw_user_meta_data->>'first_name',
     new.raw_user_meta_data->>'last_name',
-    new.email,
     new.raw_user_meta_data->>'phone_number'
   );
   return new;
